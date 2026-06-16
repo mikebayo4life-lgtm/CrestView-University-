4a945835a7ec7beecb6991f64463d1c7ce662206
pastorbams <pastorbamz@gmail.com>
2026-06-16 17:28:44 +0100

    commit

--- Admin/student.js
+++ Admin/student.js
@@ -20,6 +20,29 @@ const db = getFirestore(app);
 
 const studentTable = document.getElementById("studentTable");
 const studentCount = document.getElementById("studentCount");
+const searchInput = document.getElementById("searchStudent");
+
+let students = [];
+
+// Display students
+function displayStudents(studentArray) {
+
+    studentTable.innerHTML = "";
+
+    studentArray.forEach((student) => {
+
+        studentTable.innerHTML += `
+            <tr>
+                <td>${student.firstName} ${student.lastName}</td>
+                <td>${student.email}</td>
+                <td>${student.matricNumber}</td>
+                <td>${student.course}</td>
+            </tr>
+        `;
+
+    });
+
+}
 
 const snapshot = await getDocs(
     collection(db, "applications")
@@ -33,18 +56,35 @@ snapshot.forEach((doc) => {
 
     if (data.status === "Approved") {
 
+        students.push(data);
+
         totalStudents++;
 
-        studentTable.innerHTML += `
-            <tr>
-                <td>${data.firstName} ${data.lastName}</td>
-                <td>${data.email}</td>
-                <td>${data.matricNumber}</td>
-                <td>${data.course}</td>
-            </tr>
-        `;
     }
 
 });
 
+displayStudents(students);
+
 studentCount.innerHTML = totalStudents;
+
+// Search
+searchInput.addEventListener("input", () => {
+
+    const newSearch = searchInput.value.toLowerCase();
+
+    const filteredStudents = students.filter((student) =>
+
+        student.firstName.toLowerCase().includes(newSearch) ||
+
+        student.lastName.toLowerCase().includes(newSearch) ||
+
+        (student.matricNumber || "").toLowerCase().includes(newSearch) ||
+
+        student.course.toLowerCase().includes(newSearch)
+
+    );
+
+    displayStudents(filteredStudents);
+
+});
